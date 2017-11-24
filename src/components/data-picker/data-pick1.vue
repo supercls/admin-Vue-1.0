f9b09df44719226c26642ba598c65008310cd7d1
supercls <649251829@qq.com>
2017-11-24 10:57:52 +0800

    pikeer

---
+++ src/components/data-picker/data-pick1.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<el-date-picker
+				v-model="pickDate"
+				type="daterange"
+				align="right"
+				placeholder="选择日期范围"
+				@change="change"
+				:picker-options="pickerOptions">
+		</el-date-picker>
+	</div>
+</template>
+<script>
+export default{
+    methods:{
+        change: function (value) {
+            this.$emit('dataPick1',value)
+        }
+    },
+    data(){
+        return {
+            pickDate:'',
+            pickerOptions: {
+                shortcuts: [{
+                    text: '最近一周',
+                    onClick(picker) {
+                    const end = new Date();
+                    const start = new Date();
+                    start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                    picker.$emit('pick', [start, end]);
+                }
+                }, {
+                    text: '最近一个月', onClick(picker) {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                        picker.$emit('pick', [start, end]);
+                    }
+                }, {
+                    text: '最近三个月', onClick(picker) {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                        picker.$emit('pick', [start, end]);
+                    }
+                }]
+            },
+        }
+    },
+}
+</script>
+<style>
+
+</style>
